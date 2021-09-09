8ae6e5e61ff79be1201cbaa8cabc77f968f961f5
EvgeniaRepina <88374316+EvgeniaRepina@users.noreply.github.com>
2021-09-09 17:44:44 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int ticketCostRub = 40001;
+        int oneMileCostRub = 20;
+        int milesAmount = ticketCostRub / oneMileCostRub;
+        System.out.println(milesAmount);
+    }
+}
